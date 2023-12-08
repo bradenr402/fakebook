ec19021a7fe7bf452e82884d129b2f21b5f6bcb1
Braden <108199884+bradenr402@users.noreply.github.com>
2023-12-08 12:37:25 -0500

    add posts CRUD actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,52 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "Post successfully created"
+      redirect_to @post
+    else
+      flash[:error] = "Something went wrong"
+      render :new, staus: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:success] = "Post was successfully updated"
+      redirect_to @post
+    else
+      flash[:error] = "Something went wrong"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
