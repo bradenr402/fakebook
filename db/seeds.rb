9da364ab68796c36304a9a8c6ca091b3595db90c
Braden <108199884+bradenr402@users.noreply.github.com>
2023-12-12 14:24:48 -0500

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,42 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+puts 'Destroying old data...'
+
+User.destroy_all
+Post.destroy_all
+Comment.destroy_all
+
+puts 'Seeding new data...'
+
+puts 'Generating users...'
+User.create(name: 'Braden Roth',
+            password: 'password',
+            email: 'bradenr402@gmail.com')
+
+10.times do
+  User.create(name: Faker::Name.name,
+              # email: Faker::Internet.email(name: name, separators: ['-'], domain: 'gmail.com'),
+              password: Faker::Internet.password) do |user|
+                user.email = Faker::Internet.email(name: user.name, separators: ['_'], domain: 'gmail.com')
+                rand(3..7).times do
+                  user.posts.build(body: Faker::Lorem.paragraph_by_chars(number: rand(100..450))) do
+
+                  end
+                end
+              end
+end
+
+puts 'Generating posts...'
+User.all.each do |user|
+end
+
+puts 'Generating comments...'
+Post.all.each do |post|
+  User.all.each do |user|
+    rand(1..3).times do
+      post.comments.create(body: Faker::Lorem.paragraph_by_chars(number: rand(30..100))) do |comment|
+        comment.user = user
+      end
+    end
+  end
+end
