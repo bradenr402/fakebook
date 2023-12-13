8413f620cadf89943a0ed5af80f487032506ab7e
Braden <108199884+bradenr402@users.noreply.github.com>
2023-12-13 17:58:38 -0500

    add follows

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,59 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(user_params)
+
+    if @user.update(params[:user])
+      flash[:success] = 'User was successfully updated'
+      redirect_to @user
+    else
+      flash[:error] = 'Something went wrong'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def follow
+    @user = User.find(params[:id])
+    current_user.followees << @user
+    redirect_back(fallback_location: user_path(@user))
+  end
+
+  def unfollow
+    @user = User.find(params[:id])
+    current_user.followed_users.find_by(followee_id: @user.id).destroy
+    redirect_back(fallback_location: user_path(@user))
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followers
+    @title = 'Followers'
+
+    render 'show_follow'
+  end
+
+  def following
+    @user = User.find(params[:id])
+    @users = @user.followees
+    @title = 'Following'
+
+    render 'show_follow'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,17 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes
 
+  has_many :followed_users, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :followees, through: :followed_users
+
+  has_many :following_users, foreign_key: :followee_id, class_name: 'Follow'
+  has_many :followers, through: :following_users
+
+  validates_presence_of :name, :email
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
