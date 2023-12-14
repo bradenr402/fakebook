1f7602845b79567741d005a2b3d6b58bf97488b8
Braden <108199884+bradenr402@users.noreply.github.com>
2023-12-14 18:12:56 -0500

    add accept! and decline! methods to follow_requests

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -1,3 +1,23 @@
 class FollowRequest < ApplicationRecord
   enum :status, { pending: 0, accepted: 1, declined: 2 }
+
+  belongs_to :follower, class_name: 'User'
+  belongs_to :followee, class_name: 'User'
+
+  validates_presence_of :follower_id, :followee_id
+  validates :status, inclusion: { in: FollowRequest.statuses.keys }
+
+  def accept!
+    update!(status: 'accepted')
+    follower = User.find(follower_id)
+    followee = User.find(followee_id)
+
+    follower.follow(followee)
+
+    # send notification to follower
+  end
+
+  def decline!
+    update!(status: 'decline')
+  end
 end
