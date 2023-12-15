9a3ef588942bf64de3270178b6815d07c29e4f23
Braden <108199884+bradenr402@users.noreply.github.com>
2023-12-14 20:59:37 -0500

    rubocop fixes

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,10 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   resources :posts do
-    resources :comments, only: [:create, :destroy]
+    resources :comments, only: %i[create destroy]
   end
 
-  resources :likes, only: [:create, :destroy]
+  resources :likes, only: %i[create destroy]
 
   resources :users do
     member do
