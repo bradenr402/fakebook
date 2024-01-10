d30034b9769f4375eceb29a36a61ae1d7d2ca8c3
Braden <108199884+bradenr402@users.noreply.github.com>
2024-01-10 12:45:39 -0600

    add created_at time to posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def formatted_date(date)
+    date.strftime('%B %d, %Y')
+  end
+
+  def formatted_time(time)
+    time.strftime('%I:%M %p')
+  end
+
+  def formatted_datetime(datetime)
+    datetime.strftime('%B %d, %Y at %I:%M %p')
+  end
 end
